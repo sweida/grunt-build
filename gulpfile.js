c3ffefcfffef0248b96d6c02d7bd852c94856d19
sweida <weidaxyy@163.com>
2018-06-22 02:35:47 +0800

    gulp-demo

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp = require('gulp'), //本地安装gulp所用到的地方
+    less = require('gulp-less');
+    htmlmin = require('gulp-htmlmin');
+    cleanCSS = require('gulp-clean-css');
+    cssver = require('gulp-make-css-url-version');  // 给css文件里引用url加版本号
+
+// 压缩html
+gulp.task('testHtmlmin', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('src/*.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist'));
+});
+
+// 编译less
+gulp.task('testLess', function () {
+    gulp.src('src/less/index.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('dist/css')); //将会在src/css下生成index.css
+});
+
+// 压缩css
+gulp.task('testCssmin', function () {
+    gulp.src('src/css/*.css')
+        .pipe(cssver()) //给css文件里引用文件加版本号（文件MD5）
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default',['testLess', 'testHtmlmin', 'testCssmin']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组)
+//gulp.dest(path[, options]) 处理完后文件生成路径
